469bfe5356a8b8a3371f4911583bb0687e050c24
SuperPlane Agent <superplaneagent@superplane.com>
2026-08-31 21:17:29 +0000

    api: annotate Test__CreateDeck with doc comment
    Signed-off-by: SuperPlane Agent <superplaneagent@superplane.com>

--- pkg/api/server_test.go
+++ pkg/api/server_test.go
@@ -21,6 +21,7 @@ func Test__HealthCheckEndpointRespondsWith200(t *testing.T) {
 	require.Equal(t, response.Code, 200)
 }
 
+// Test__CreateDeck exercises the deck creation endpoint.
 func Test__CreateDeck(t *testing.T) {
 	testServer := NewServerrrrrrr(storage.NewInMemoryStorage())
 
