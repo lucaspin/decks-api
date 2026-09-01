4f112da2ad869fbb02c4ea7996fcb31a74c58ccc
SuperPlane Agent <superplaneagent@superplane.com>
2026-09-01 12:49:32 +0000

    fix(api): correct NewServer call in CreateDeck test
    Signed-off-by: SuperPlane Agent <superplaneagent@superplane.com>

--- pkg/api/server_test.go
+++ pkg/api/server_test.go
@@ -22,7 +22,7 @@ func Test__HealthCheckEndpointRespondsWith200(t *testing.T) {
 }
 
 func Test__CreateDeck(t *testing.T) {
-	testServer := NewServerrrrr(storage.NewInMemoryStorage())
+	testServer := NewServer(storage.NewInMemoryStorage())
 
 	t.Run("default deck created", func(t *testing.T) {
 		response := execRequest(testServer, http.MethodPost, "/api/v1alpha/decks", nil)
